83f0c7f6631e25936db2cdaa5ab0c968a5d42240
oxygen <oxygenall@yahoo.com>
2020-08-22 15:59:49 +0200

    update

--- js/main.js
+++ js/main.js
@@ -12,7 +12,13 @@ var app = new Vue({
         reposts: false,
         teaser: false,
         visual: false,
-        title: "Osciallate - SoundCloud Embeds"
+        title: "Osciallate - SoundCloud Embeds",
+        sharing: false,
+        download: false,
+        buying: false,
+        show_artwork: false,
+        show_playcount: false,
+        single_active: false,
 
     },
     methods: {
@@ -22,7 +28,7 @@ var app = new Vue({
     },
     computed: {
         apistring: function () {
-            return `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${this.track}&color=%23${this.color.substring(1)}&auto_play=${this.autoplay}&hide_related=${this.related}&show_comments=${this.comments}&show_user=${this.user}&show_reposts=${this.reposts}&show_teaser=${this.teaser}&visual=${this.visual}`
+            return `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${this.track}&color=%23${this.color.substring(1)}&buying=${this.buying}&sharing=${this.sharing}&download=${this.download}&show_artwork=${this.show_artwork}&show_playcount=${this.show_playcount}&single_active=${this.single_active}&auto_play=${this.autoplay}&hide_related=${this.related}&show_comments=${this.comments}&show_user=${this.user}&show_reposts=${this.reposts}&show_teaser=${this.teaser}&visual=${this.visual}`
         },
         embedshowcase: function () {
             return `<iframe width="${this.width}" height="${this.height}" scrolling="on" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${this.track}&color=%23${this.color.substring(1)}&auto_play=${this.autoplay}&hide_related=${this.related}&show_comments=${this.comments}&show_user=${this.user}&show_reposts=${this.reposts}&show_teaser=${this.teaser}&visual=${this.visual}"></iframe>`
